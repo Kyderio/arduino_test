907b4041fb7a10f4ed1ed883ed667b26d97a2f49
kevin_si <kevin_si@realsil.com.cn>
2025-12-05 11:15:43 +0800

    Modify SimplyPrint example info
    Change-Id: I2e16cccd31ff5bb5e359d9fccb0a1d2ddd5e42f8

--- Arduino_package/hardware/libraries/Print/examples/SimplyPrint/SimplyPrint.ino
+++ Arduino_package/hardware/libraries/Print/examples/SimplyPrint/SimplyPrint.ino
@@ -1,19 +1,5 @@
 /*
- This example prints MAC address, and
- scans for available Wifi networks.
- Every ten seconds, it scans again. It doesn't actually
- connect to any network, so no encryption scheme is specified.
-
- created 13 July 2010
- by dlf (Metodo2 srl)
- modified 21 Junn 2012
- by Tom Igoe and Jaymes Dec
-
- modified 08 May 2023
- by Realtek SG
-
- Example guide:
- https://www.amebaiot.com/en/amebad-arduino-scan-wifi/
+ This example only print Hello Arduino every 1s.
  */
 
 #include <Arduino.h>
